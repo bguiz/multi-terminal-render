1c7d23c87737f42b667bfd4d0676dac61ae94ef8
bguiz <bguiz@users.noreply.github.com>
2020-06-25 12:01:03 +0800

    feat: impl ability to duplicate same content across multiple tabs using ', ' as a separator

--- index.js
+++ index.js
@@ -49,61 +49,85 @@ function renderMultipleTerminalsListElem(ul, li, liIdx) {
     );
     return;
   }
-  const tabText = li.childNodes[0].textContent.trim();
-  const tabTextId = tabText.toLowerCase();
-  if (['linux', 'mac', 'windows'].indexOf(tabTextId) < 0) {
-    console.warn(
-      `Child element #${liIdx} does not reference a supported OS terminal.`,
-      ul,
-      li,
+  var tabText = li.childNodes[0].textContent.trim();
+  var oses = tabText.split(', ').map(function (s) { return s.trim(); });
+  oses.forEach(function renderMultipleTerminalsListElemOs(osText, osIdx) {
+    var os = (osText.split(' ')[0]).toLowerCase();
+    if (['linux', 'mac', 'windows'].indexOf(os) < 0) {
+      console.warn(
+        `Child element #${liIdx} does not reference a supported OS terminal.`,
+        ul,
+        li,
+      );
+      return;
+    }
+    var isLastOs = (osIdx === oses.length - 1);
+    var tab = li;
+
+    // create a tabTitle <span> to replace the text node in <li>,
+    // in order to apply classes
+    var tabTitle = document.createElement('span');
+    tabTitle.textContent = osText;
+    tabTitle.classList.add('multi-terminal-tabtitle');
+    tabTitle.classList.add(`multi-terminal-tabtitle-${os}`);
+    tabTitle.setAttribute('data-os', os);
+    if (isLastOs) {
+      tab.replaceChild(tabTitle, tab.childNodes[0]);
+    } else {
+      // we have to create a new list element as well
+      var newTab = document.createElement('li');
+      newTab.appendChild(tabTitle);
+      if (tab.nextSibling) {
+        tab.parentNode.insertBefore(
+          newTab,
+          tab,
+        );
+      } else {
+        tab.parentNode.appendChild(tabTitle);
+      }
+      tab = newTab;
+    }
+
+    // create a new tabContent <div>
+    // and move rest of contents of the <li> into this
+    var tabContent = document.createElement('div');
+    tabContent.classList.add('multi-terminal-tabcontent');
+    tabContent.classList.add(`multi-terminal-tabcontent-${os}`);
+    tabContent.setAttribute('data-os', os);
+    for (let childIdx = 1; childIdx < li.childNodes.length; ++childIdx) {
+      if (isLastOs) {
+        tabContent.appendChild(li.childNodes[childIdx]);
+      } else {
+        tabContent.appendChild(li.childNodes[childIdx].cloneNode(true));
+      }
+    }
+
+    // place the tabContent <div> immediately subsequent to the <ul>
+    // to which this <li> belongs
+    // also set the 1st one among them to be active,
+    // otherwise none will be visible by default
+    tab.classList.add('multi-terminal-tab');
+    tab.classList.add(`multi-terminal-tab-${os}`);
+    tab.setAttribute('data-os', os);
+    var isFirstTab = (liIdx === 0 && osIdx === 0);
+    tab.classList.toggle('active', isFirstTab);
+    tabTitle.classList.toggle('active', isFirstTab);
+    tabContent.classList.toggle('active', isFirstTab);
+    ul.parentNode.insertBefore(
+      tabContent,
+      ul.nextSibling,
     );
-    return;
-  }
-
-  // create a tabTitle <span> to replace the text node in <li>,
-  // in order to apply classes
-  const tabTitle = document.createElement('span');
-  tabTitle.textContent = tabText;
-  tabTitle.classList.add('multi-terminal-tabtitle');
-  tabTitle.classList.add(`multi-terminal-tabtitle-${tabTextId}`);
-  tabTitle.setAttribute('data-os', tabTextId);
-  li.replaceChild(tabTitle, li.childNodes[0]);
-
-  // create a new tabContent <div>
-  // and move rest of contents of the <li> into this
-  const tabContent = document.createElement('div');
-  tabContent.classList.add('multi-terminal-tabcontent');
-  tabContent.classList.add(`multi-terminal-tabcontent-${tabTextId}`);
-  tabContent.setAttribute('data-os', tabTextId);
-  for (let childIdx = 1; childIdx < li.childNodes.length; ++childIdx) {
-    tabContent.appendChild(li.childNodes[childIdx]);
-  }
-
-  // place the tabContent <div> immediately subsequent to the <ul>
-  // to which this <li> belongs
-  // also set the 1st one among them to be active,
-  // otherwise none will be visible by default
-  li.classList.add('multi-terminal-tab');
-  li.classList.add(`multi-terminal-tab-${tabTextId}`);
-  li.setAttribute('data-os', tabTextId);
-  const isFirstTab = (liIdx === 0);
-  li.classList.toggle('active', isFirstTab);
-  tabTitle.classList.toggle('active', isFirstTab);
-  tabContent.classList.toggle('active', isFirstTab);
-  ul.parentNode.insertBefore(
-    tabContent,
-    ul.nextSibling,
-  );
+  });
 }
 
 function renderMultipleTerminalsOnClickTabTitle (e) {
-  const tabTitle = e.target;
+  var tabTitle = e.target;
   if (tabTitle.classList.contains('multi-terminal-tabtitle')) {
-    const tab = tabTitle.parentNode;
-    const tabTextId = tab.getAttribute('data-os');
-    const allTabsNodeList =
+    var tab = tabTitle.parentNode;
+    var tabTextId = tab.getAttribute('data-os');
+    var allTabsNodeList =
       document.querySelectorAll('.multi-terminal-tab');
-    const allTabs =
+    var allTabs =
       Array.prototype.slice.call(allTabsNodeList);
     allTabs.forEach(function (currTab) {
       currTab.classList.toggle(
@@ -111,9 +135,9 @@ function renderMultipleTerminalsOnClickTabTitle (e) {
         (currTab.getAttribute('data-os') === tabTextId),
       );
     });
-    const allTabsContentNodeList =
+    var allTabsContentNodeList =
       document.querySelectorAll('.multi-terminal-tabcontent');
-    const allTabsContent =
+    var allTabsContent =
       Array.prototype.slice.call(allTabsContentNodeList);
     allTabsContent.forEach(function (currTabContent) {
       currTabContent.classList.toggle(
@@ -125,7 +149,7 @@ function renderMultipleTerminalsOnClickTabTitle (e) {
 }
 
 function renderCustomTerminalsFrames() {
-  const elemNodeList =
+  var elemNodeList =
     document.querySelectorAll('.language-windows-command-prompt');
   var elems = Array.prototype.slice.call(elemNodeList);
   elems.forEach(function (el) {
